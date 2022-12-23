c50e99b3075d8cb5529538c594d5d390c33d1f67
Kayle <italo.santos02@outlook.com.br>
2022-12-23 18:04:20 -0300

    Adição do projeto de teste de lista

---
+++ TestBase/ControlExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TestBase.ControlExtension
+{
+    public class TextExtensor
+    {
+        public static string Clear(TextBox text) => text.Text = string.Empty;
+        public static void CountChanger(TextBlock number, string signal) 
+        {
+            int actualNumber;
+            int.TryParse(number.Text, out actualNumber);
+            if (signal == "+") actualNumber++;
+            else if (signal == "-") actualNumber--;
+            number.Text = actualNumber.ToString();
+        }
+    }
+}
---
+++ TestBase/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using TestBase.ControlExtension;
+
+
+namespace TestBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtName.Text) && !lstName.Items.Contains(txtName.Text))
+            {
+                lstName.Items.Add(txtName.Text);
+                TextExtensor.Clear(txtName);
+                TextExtensor.CountChanger(DisplayName, "+");
+            }
+        }
+
+        private void btnRmvName_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstName.SelectedIndex != -1) 
+            {
+                var value = lstName.SelectedValue;
+                lstName.Items.Remove(value);
+                TextExtensor.CountChanger(DisplayName, "-");
+            }
+        }
+    }
+}
